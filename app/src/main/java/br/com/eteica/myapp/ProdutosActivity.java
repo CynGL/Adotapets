eedfbc75019b41fd421b1aac4d5a15505846b496
CynGL <cyntialino09@gmail.com>
2023-05-18 21:07:26 -0300

    menu

---
+++ app/src/main/java/br/com/eteica/myapp/ProdutosActivity.java
@@ -0,0 +1,14 @@
+package br.com.eteica.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProdutosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.produtos_layout);
+    }
+}
