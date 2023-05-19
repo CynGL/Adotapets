7e1b6444b22825169567d5f47832e535e92d703c
CynGL <cyntialino09@gmail.com>
2023-05-18 21:23:26 -0300

    menu

--- app/src/main/java/br/com/eteica/myapp/MenuActivity.java
+++ app/src/main/java/br/com/eteica/myapp/MenuActivity.java
@@ -3,12 +3,31 @@ package br.com.eteica.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MenuActivity extends AppCompatActivity {
+import com.google.android.material.card.MaterialCardView;
+
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
+    MaterialCardView idMenuPets, idMenuProdutos, idMenuDoacoes, idMenuServicos, idMenuLocalizacao, idMenuParceiros;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
+
+        idMenuPets = findViewById(R.id.idMenuPets);
+        idMenuProdutos = findViewById(R.id.idMenuProdutos);
+        idMenuDoacoes = findViewById(R.id.idDoacoes);
+        idMenuServicos = findViewById(R.id.idMenuServicos);
+        idMenuLocalizacao = findViewById(R.id.idMenuLocalizacao);
+        idMenuParceiros = findViewById(R.id.idMenuParceiros);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
