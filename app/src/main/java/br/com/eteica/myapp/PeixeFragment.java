b4491aa3cf0ed2f65a20b4b4ebf52f910d043a54
CynGL <cyntialino09@gmail.com>
2023-05-25 20:12:11 -0300

    fragments

---
+++ app/src/main/java/br/com/eteica/myapp/PeixeFragment.java
@@ -0,0 +1,22 @@
+package br.com.eteica.myapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PeixeFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_peixe, container, false);
+
+        return view;
+    }
+}
